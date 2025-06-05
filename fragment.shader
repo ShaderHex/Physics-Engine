a3386c43cf1e3e0e91904f1635338944ae3050a9
ShaderHex <barkott9@gmail.com>
2025-06-05 23:03:57 +0300

    Added Circles
    Camera
    
    Multiple Renderer

--- fragment.shader
+++ fragment.shader
@@ -1,5 +1,6 @@
 #version 330 core
+uniform vec4 uColor;
 out vec4 FragColor;
 void main() {
-    FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+    FragColor = uColor;
 }
--- main.cpp
+++ main.cpp
@@ -3,6 +3,37 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+float screenWidth = 1600, screenHeight = 800;
+
+static GLuint VBO = 0, VAO = 0;
+
+struct Color {
+    float r;
+    float g;
+    float b;
+    float a;
+};
+
+glm::mat4 CreateCamera(float screenWid, float screenHei) {
+    return glm::ortho(0.0f, screenWid, 0.0f, screenHei);
+}
+
+void cameraUpdate(GLuint shaderProgram, float screenWid, float screenHei) {
+    glm::mat4 projection = CreateCamera(screenWidth, screenHeight);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 model = glm::mat4(1.0f);
+
+    glm::mat4 mvp = projection * view * model;
+
+    GLint mvpLoc = glGetUniformLocation(shaderProgram, "uMVP");
+    glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
+}
 
 const char* LoadShaderFromFile(const char* fileName) {
     std::ifstream inputFile(fileName);
@@ -47,19 +78,106 @@ GLuint createShaderProgram() {
     return shaderProgram;
 }
 
-int main() {
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
+void drawTriangle(GLuint shaderProgram, float posX, float posY, float scaleX, float scaleY, Color color) {
+    
+    if (VAO == 0) {
+        float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+        };
 
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glBindVertexArray(0);
+    }
+
+    glUseProgram(shaderProgram);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(posX, posY, 0.0f));
+    model = glm::scale(model, glm::vec3(scaleX, scaleY, 1.0f));
+
+    glm::mat4 projection = glm::ortho(0.0f, screenWidth, 0.0f, screenHeight);
+    glm::mat4 view = glm::mat4(1.0f);
+
+    glm::mat4 mvp = projection * view * model;
+
+    GLint mvpLoc = glGetUniformLocation(shaderProgram, "uMVP");
+    glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
+
+    GLuint colorLoc = glGetUniformLocation(shaderProgram, "uColor");
+    glUniform4f(colorLoc, color.r, color.g, color.b, color.a);
+
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void drawCircle(GLuint shaderProgram, float cx, float cy, float radius, Color color, int segments = 32) {
+    static std::vector<float> vertices;
+    static GLuint VAO = 0, VBO = 0;
+
+    vertices.clear();
+    vertices.push_back(0.0f);
+    vertices.push_back(0.0f);
+    vertices.push_back(0.0f);
+
+    for (int i = 0; i <= segments; i++) {
+        float angle = i * 2.0f * 3.14159f / segments;
+        float x = cosf(angle) * radius;
+        float y = sinf(angle) * radius;
+        vertices.push_back(x);
+        vertices.push_back(y);
+        vertices.push_back(0.0f);
+    }
+
+    if (VAO == 0) {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+    }
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glUseProgram(shaderProgram);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(cx, cy, 0.0f));
+    glm::mat4 projection = glm::ortho(0.0f, screenWidth, 0.0f, screenHeight);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 mvp = projection * view * model;
+
+    GLint mvpLoc = glGetUniformLocation(shaderProgram, "uMVP");
+    glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
+
+    GLint colorLoc = glGetUniformLocation(shaderProgram, "uColor");
+    glUniform4f(colorLoc, color.r, color.g, color.b, color.a);
+
+    glDrawArrays(GL_TRIANGLE_FAN, 0, segments + 2);
+
+    glBindVertexArray(0);
+}
+
+
+int main() {
     if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    GLFWwindow* window = glfwCreateWindow(1600, 900, "Physics", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Physics", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -72,21 +190,6 @@ int main() {
         return -1;
     }
 
-    GLuint VBO;
-    GLuint VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
     GLuint shaderProgram = createShaderProgram();
 
     glClearColor(0.2, 0.3, 0.3, 1);
@@ -94,14 +197,14 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        cameraUpdate(shaderProgram, screenWidth, screenHeight);
+        drawTriangle(shaderProgram, 100.0f, 100.0f, 200.0f, 200.0f,{0.5, 1, 0.3, 1});
+        drawCircle(shaderProgram, 300.0f, 300.0f, 100.0f, {1, 0, 0, 1});
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
