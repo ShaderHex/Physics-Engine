08fd281b8126cb666f53133d829e22a194b601fa
ShaderHex <barkott9@gmail.com>
2025-06-06 19:35:27 +0300

    Added the physics

--- Makefile
+++ Makefile
@@ -1,27 +1,28 @@
 CXX = g++
 CXXFLAGS = -std=c++17 -O2
 
-# Source files
-SRC = main.cpp glad/glad.c
+SRC = main.cpp glad/glad.c \
+      imgui/imgui.cpp \
+      imgui/imgui_draw.cpp \
+      imgui/imgui_tables.cpp \
+      imgui/imgui_widgets.cpp \
+      imgui/imgui_demo.cpp \
+      imgui/backends/imgui_impl_glfw.cpp \
+      imgui/backends/imgui_impl_opengl3.cpp
+
 OUT = app
 
-# Include the 'glad' folder (where glad.h and gl.h are)
-INCLUDES = -Iglad
+INCLUDES = -Iglad -Iimgui -Iimgui/backends
 
-# Link GLFW, OpenGL, and dl
 LIBS = -lglfw -ldl -lGL
 
-# Default target
 all: $(OUT)
 
-# Build rule
 $(OUT): $(SRC)
 	$(CXX) $(CXXFLAGS) -o $(OUT) $(SRC) $(INCLUDES) $(LIBS)
 
-# Run the app
 run: $(OUT)
 	./$(OUT)
 
-# Clean build artifacts
 clean:
 	rm -f $(OUT) *.o
--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <stdio.h>
@@ -10,8 +14,9 @@
 #include <glm/gtc/type_ptr.hpp>
 
 float screenWidth = 1600, screenHeight = 800;
-
 static GLuint VBO = 0, VAO = 0;
+float deltaTime = 1.0f / 60.0f;
+bool isPhysicsPaused = false;
 
 struct Color {
     float r;
@@ -19,6 +24,21 @@ struct Color {
     float b;
     float a;
 };
+
+struct PhysicsCircle {
+    glm::vec2 position;
+    glm::vec2 velocity;
+    float radius;
+    Color color;
+};
+
+struct StaticLine {
+    glm::vec4 position;
+    Color color;
+};
+
+std::vector<PhysicsCircle> circles;
+std::vector<StaticLine> lines;
 
 glm::mat4 CreateCamera(float screenWid, float screenHei) {
     return glm::ortho(0.0f, screenWid, 0.0f, screenHei);
@@ -170,8 +190,85 @@ void drawCircle(GLuint shaderProgram, float cx, float cy, float radius, Color co
     glBindVertexArray(0);
 }
 
+void drawLine(GLuint shaderProgram, float x1, float y1, float x2, float y2, Color color, float thickness = 1.0f) {
+    static GLuint VAO = 0, VBO = 0;
+
+    float vertices[] = {
+        x1, y1, 0.0f,
+        x2, y2, 0.0f
+    };
+
+    if (VAO == 0) {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+    }
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glUseProgram(shaderProgram);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 projection = glm::ortho(0.0f, screenWidth, 0.0f, screenHeight);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 mvp = projection * view * model;
+
+    GLint mvpLoc = glGetUniformLocation(shaderProgram, "uMVP");
+    glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
+
+    GLint colorLoc = glGetUniformLocation(shaderProgram, "uColor");
+    glUniform4f(colorLoc, color.r, color.g, color.b, color.a);
+
+    glLineWidth(thickness);
+    glDrawArrays(GL_LINES, 0, 2);
+
+    glBindVertexArray(0);
+}
+
+void createPhysicsCircle(float cirX, float cirY, float radius, Color color) {
+    PhysicsCircle c;
+    c.position = glm::vec2(cirX, cirY);
+    c.velocity = glm::vec2(0, 0);
+    c.radius = radius;
+    c.color = color;
+
+    circles.push_back(c);
+}
+
+void createStaticLine(float lineX1, float lineY1, float lineX2, float lineY2, Color color, StaticLine l) {
+    l.position = glm::vec4(lineX1, lineY1, lineX2, lineY2);
+    l.color = color;
+
+    lines.push_back(l);
+}
+
+void updatePhysicsObjects() {
+    glm::vec2 gravity(0.0f, -1200.0f);
+
+    for (auto& c : circles) {
+        c.velocity += gravity * deltaTime;
+        c.position += c.velocity * deltaTime;
+
+        for (auto& l : lines) {
+            float floorY = l.position.y;
+            if (c.position.y - c.radius < floorY) {
+                c.position.y = floorY + c.radius;
+                c.velocity.y *= -0.7f;
+
+                if (fabs(c.velocity.y) < 5.0f)
+                    c.velocity.y = 0;
+            }
+        }
+    }
+}
+
 
 int main() {
+    StaticLine l;
     if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -194,16 +291,74 @@ int main() {
 
     glClearColor(0.2, 0.3, 0.3, 1);
     
+    createPhysicsCircle(300, 600, 100, {1, 0, 1, 1});
+    createPhysicsCircle(300, 600, 100, {1, 0, 1, 1});
+    createStaticLine(10, 300, 300, 300, {0, 1, 0, 1}, l);
+    createStaticLine(10, 200, 300, 300, {0, 1, 0, 1}, l);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
         cameraUpdate(shaderProgram, screenWidth, screenHeight);
-        drawTriangle(shaderProgram, 100.0f, 100.0f, 200.0f, 200.0f,{0.5, 1, 0.3, 1});
-        drawCircle(shaderProgram, 300.0f, 300.0f, 100.0f, {1, 0, 0, 1});
+
+        for (auto& c : circles) {
+            drawCircle(shaderProgram, c.position.x, c.position.y, c. radius, c.color);
+        }
+
+        for (auto& l : lines) {
+            drawLine(shaderProgram, l.position.x, l.position.y, l.position.z, l.position.w, l.color, 5.0f);
+        }
+
+        if (isPhysicsPaused) {
+
+        } else {
+            updatePhysicsObjects();
+        }
+
+        ImGui::Begin("Inspector");
+            ImGui::Text("Enable/Disable physics");
+            
+            if (ImGui::Button("Enable / Disable")) {
+                isPhysicsPaused = !isPhysicsPaused;
+            }
+            
+            int circleIndex = 0;
+            for (auto& c : circles) {
+                ImGui::Text("Circle %d Position", circleIndex);
+                ImGui::SliderFloat(("Y##" + std::to_string(circleIndex)).c_str(), &c.position.y, 0, 801);
+                ImGui::SliderFloat(("X##" + std::to_string(circleIndex)).c_str(), &c.position.x, 0, 1601);
+                circleIndex++;
+            }
+
+
+            for (auto& l : lines) {
+                ImGui::Text("Line Position");
+            }
+
+        ImGui::End();
+        
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
